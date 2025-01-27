5138bb3b702575857f2126dea4b60b6e7a04affc
Arjun Subramanya <98678018+arjun2126@users.noreply.github.com>
2025-01-27 10:11:50 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    port: process.env.PORT || 5173,
+    host: '0.0.0.0'
+  }
 })
